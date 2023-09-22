8b6cda50c17ea0b0f84c1e44d78d2dd4ecc1d85e
DaggupatiPavan <97659712+DaggupatiPavan@users.noreply.github.com>
2023-09-22 11:57:05 +0530

    Update backend.tf

--- pub_instance/backend.tf
+++ pub_instance/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "pe2dev"
-    key    = "platform_Enginnering/public_Instance/terraform.tfstate"
+    key    = "platform_Enginnering/public_Instance/xyz1/terraform.tfstate"
     region = "us-east-1"
   }
 }
