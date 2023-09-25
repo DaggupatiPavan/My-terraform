04dcd7f3db340420633b39808128045eda0ab9c8
DaggupatiPavan <97659712+DaggupatiPavan@users.noreply.github.com>
2023-09-25 16:30:27 +0530

    Update vpc_sg.tf

--- vpc/vpc_sg.tf
+++ vpc/vpc_sg.tf
@@ -1,7 +1,7 @@
 resource "aws_security_group" "VPC_SG" {
   name        = "allow_tls"
   description = "Allow TLS inbound traffic"
-  vpc_id      = aws_vpc.main.id
+  vpc_id      = module.vpc.vpc_id
 
   ingress {
     description      = "TLS from VPC"
