cd0486550e255443eaa4ef52d5d23ca184fc152a
DaggupatiPavan <97659712+DaggupatiPavan@users.noreply.github.com>
2023-09-26 12:20:18 +0530

    Update securityGroup.tf

--- pri_instance/securityGroup.tf
+++ pri_instance/securityGroup.tf
@@ -1,32 +1,3 @@
-/* data "terraform_remote_state" "vpc" {
-  backend = "local"
-  config = {
-    path = "../vpc/terraform.tfstate"
-  }
-} */
-
-/* resource "aws_security_group" "security_sg" {
-    name        = var.security_sg_pri_name
-    description = "Allow HTTPS to web server"  #data.terraform_remote_state.vpc.outputs.default_security_group_id
-    vpc_id      = data.terraform_remote_state.vpc.outputs.vpc_id
-
-    ingress {
-        description = "HTTPS ingress"
-        from_port   = 22
-        to_port     = 22
-        protocol    = "tcp"
-        cidr_blocks = [
-            data.terraform_remote_state.vpc.outputs.vpc_cidr_block
-        ]
-    }
-    egress {
-        from_port   = 0
-        to_port     = 0
-        protocol    = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
-   }
-} */
-
 
 resource "aws_security_group" "security_sg"  {
     name        = "example-sg"
@@ -36,10 +7,10 @@ resource "aws_security_group" "security_sg"  {
 
 # Inbound rules
 resource "aws_security_group_rule" "inbound_rules" {
-    count = 2  
+    count = 6 
     type        = "ingress"
-    from_port   = element([22, 80], count.index)
-    to_port     = element([22, 80], count.index)
+    from_port   = element([22, 80, 8080, 9000, 9090, 3000], count.index)
+    to_port     = element([22, 80, 8080, 9000, 9090, 3000], count.index)
     protocol    = "tcp"
     cidr_blocks = [data.terraform_remote_state.vpc.outputs.vpc_cidr_block]
     security_group_id = aws_security_group.security_sg.id
@@ -54,4 +25,3 @@ resource "aws_security_group_rule" "egress_rules" {
     cidr_blocks = ["0.0.0.0/0"]
     security_group_id = aws_security_group.security_sg.id
 }
-
