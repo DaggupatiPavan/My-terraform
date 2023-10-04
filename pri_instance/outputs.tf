8f835079df1e614f41f6612aa770150de3554150
DaggupatiPavan <97659712+DaggupatiPavan@users.noreply.github.com>
2023-10-04 14:59:40 +0530

    Update outputs.tf

--- pri_instance/outputs.tf
+++ pri_instance/outputs.tf
@@ -5,6 +5,3 @@ output "instance_IDs" {
   value = aws_instance.priv_instance[*].id
 }
 
-output "s3_backend_path" {
-  value = "s3://${terraform.backend["s3"].bucket}/${terraform.backend["s3"].key}"
-}
